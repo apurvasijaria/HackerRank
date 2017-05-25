346d4e605f4138f9b20f4068b05504b01adfca7c
Apurva Sijaria <apurvasijaria.iitkgp@gmail.com>
2017-05-25 15:47:10 +0530

    Create solve-me-first.py

---
+++ solve-me-first.py
@@ -0,0 +1,12 @@
+#https://www.hackerrank.com/challenges/solve-me-first
+
+def solveMeFirst(a,b):
+    c=a+b
+    return c
+  
+
+
+num1 = input()
+num2 = input()
+res = solveMeFirst(num1,num2)
+print res
